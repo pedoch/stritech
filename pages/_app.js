b1b5e287d4e0d8d479a5263bcffb14a7cee584c2
PedroOgheneochuko <pedroogheneochuko@gmail.com>
2020-05-08 09:45:52 +0100

    add nprogress and lower resolution picture

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,26 @@
 import 'antd/dist/antd.css';
-// import '../static/css/index.css';
+import '../static/css/nprogress.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import '../components/tabs/tab.module.css';
 import '../static/css/antdoverwrite.scss';
 import App from 'next/app';
 import Head from 'next/head';
-// import Router from 'next/router';
-// import NProgress from 'nprogress';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import React from 'react';
 
-// const showProgressBar = (delay) => {
-// 	const timer = setTimeout(() => NProgress.start(), delay);
-// 	Router.events.on('routeChangeComplete', () => stopProgress(timer));
-// 	Router.events.on('routeChangeError', () => stopProgress(timer));
-// };
+const showProgressBar = (delay) => {
+	const timer = setTimeout(() => NProgress.start(), delay);
+	Router.events.on('routeChangeComplete', () => stopProgress(timer));
+	Router.events.on('routeChangeError', () => stopProgress(timer));
+};
 
-// const stopProgress = (timer) => {
-// 	clearTimeout(timer);
-// 	NProgress.done();
-// };
+const stopProgress = (timer) => {
+	clearTimeout(timer);
+	NProgress.done();
+};
 
-// Router.events.on('routeChangeStart', () => showProgressBar(500));
+Router.events.on('routeChangeStart', () => showProgressBar(500));
 
 class MyApp extends App {
 	render() {
